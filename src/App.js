c37cf9b36c51d70e55b70bcaa24105f6d2a8b377
David Gómez <david.gomez@beezy.net>
2017-12-19 11:51:37 +0100

    Apply fetch

--- src/App.js
+++ src/App.js
@@ -4,11 +4,38 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      querySearch: '',
+      results: []
+    };
+  }
+  search = () => {
+    const searchQuery = this.state.querySearch;
+    fetch(`https://api.cognitive.microsoft.com/bing/v7.0/search?q=${this.state.querySearch}`, {
+      headers: {
+        "Ocp-Apim-Subscription-Key": "d1164832b71449cebc47ee3fe7b565b6"
+      },
+      mode: "cors"
+    }).then(function(response) {
+      return response.json();
+    }).then((results) => {
+      this.setState({
+        results
+      });
+    })
+  }
+  onChangeInput = (e) => {
+    this.setState({
+      querySearch: e.target.value
+    })
+  }
   render() {
     return (
       <div>
-        <Input placeholder="topic" />
-        <Button color="primary" onClick="">primary</Button>
+        <Input placeholder="topic" onChange={this.onChangeInput} />
+        <Button color="primary" onClick={this.search} >primary</Button>
       </div>
     );
   }

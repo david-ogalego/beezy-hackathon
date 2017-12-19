3d14cdb3d737b05dbe33cb4f683e5468edd7e78e
David Gómez <david.gomez@beezy.net>
2017-12-19 12:19:07 +0100

    Api Bing search working

--- src/App.js
+++ src/App.js
@@ -4,125 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import List from './List';
 import './App.css';
 
-const moskResult = {
-  "_type": "SearchResponse",
-  "queryContext": {
-    "originalQuery": "Microsoft Cognitive Services"
-  },
-  "webPages": {
-    "webSearchUrl": "https://www.bing.com/search?q=Microsoft+cognitive+services",
-    "totalEstimatedMatches": 22300000,
-    "value": [
-      {
-        "id": "https://api.cognitive.microsoft.com/api/v7/#WebPages.0",
-        "name": "Microsoft Cognitive Services",
-        "url": "https://www.microsoft.com/cognitive-services",
-        "displayUrl": "https://www.microsoft.com/cognitive-services",
-        "snippet": "Knock down barriers between you and your ideas. Enable natural and contextual interaction with tools that augment users' experiences via the power of machine-based AI. Plug them in and bring your ideas to life.",
-        "deepLinks": [
-          {
-            "name": "Face API",
-            "url": "https://azure.microsoft.com/en-us/services/cognitive-services/face/",
-            "snippet": "Add facial recognition to your applications to detect, identify, and verify faces using a Face API from Microsoft Azure. ... Cognitive Services; Face API;"
-          },
-          {
-            "name": "Text Analytics",
-            "url": "https://azure.microsoft.com/en-us/services/cognitive-services/text-analytics/",
-            "snippet": "Cognitive Services; Text Analytics API; Text Analytics API . Detect sentiment, ... you agree that Microsoft may store it and use it to improve Microsoft services, ..."
-          },
-          {
-            "name": "Computer Vision API",
-            "url": "https://azure.microsoft.com/en-us/services/cognitive-services/computer-vision/",
-            "snippet": "Extract the data you need from images using optical character recognition and image analytics with Computer Vision APIs from Microsoft Azure."
-          },
-          {
-            "name": "Emotion",
-            "url": "https://www.microsoft.com/cognitive-services/en-us/emotion-api",
-            "snippet": "Cognitive Services Emotion API - microsoft.com"
-          },
-          {
-            "name": "Bing Speech API",
-            "url": "https://azure.microsoft.com/en-us/services/cognitive-services/speech/",
-            "snippet": "Add speech recognition to your applications, including text to speech, with a speech API from Microsoft Azure. ... Cognitive Services; Bing Speech API;"
-          },
-          {
-            "name": "Get Started for Free",
-            "url": "https://azure.microsoft.com/en-us/services/cognitive-services/",
-            "snippet": "Add vision, speech, language, and knowledge capabilities to your applications using intelligence APIs and SDKs from Cognitive Services."
-          }
-        ]
-      }
-    ]
-  },
-  "relatedSearches": {
-    "id": "https://api.cognitive.microsoft.com/api/v7/#RelatedSearches",
-    "value": [
-      {
-        "text": "microsoft bot framework",
-        "displayText": "microsoft bot framework",
-        "webSearchUrl": "https://www.bing.com/search?q=microsoft+bot+framework"
-      },
-      {
-        "text": "microsoft cognitive services youtube",
-        "displayText": "microsoft cognitive services youtube",
-        "webSearchUrl": "https://www.bing.com/search?q=microsoft+cognitive+services+youtube"
-      },
-      {
-        "text": "microsoft cognitive services search api",
-        "displayText": "microsoft cognitive services search api",
-        "webSearchUrl": "https://www.bing.com/search?q=microsoft+cognitive+services+search+api"
-      },
-      {
-        "text": "microsoft cognitive services news",
-        "displayText": "microsoft cognitive services news",
-        "webSearchUrl": "https://www.bing.com/search?q=microsoft+cognitive+services+news"
-      },
-      {
-        "text": "ms cognitive service",
-        "displayText": "ms cognitive service",
-        "webSearchUrl": "https://www.bing.com/search?q=ms+cognitive+service"
-      },
-      {
-        "text": "microsoft cognitive services text analytics",
-        "displayText": "microsoft cognitive services text analytics",
-        "webSearchUrl": "https://www.bing.com/search?q=microsoft+cognitive+services+text+analytics"
-      },
-      {
-        "text": "microsoft cognitive services toolkit",
-        "displayText": "microsoft cognitive services toolkit",
-        "webSearchUrl": "https://www.bing.com/search?q=microsoft+cognitive+services+toolkit"
-      },
-      {
-        "text": "microsoft cognitive services api",
-        "displayText": "microsoft cognitive services api",
-        "webSearchUrl": "https://www.bing.com/search?q=microsoft+cognitive+services+api"
-      }
-    ]
-  },
-  "rankingResponse": {
-    "mainline": {
-      "items": [
-        {
-          "answerType": "WebPages",
-          "resultIndex": 0,
-          "value": {
-            "id": "https://api.cognitive.microsoft.com/api/v7/#WebPages.0"
-          }
-        }
-      ]
-    },
-    "sidebar": {
-      "items": [
-        {
-          "answerType": "RelatedSearches",
-          "value": {
-            "id": "https://api.cognitive.microsoft.com/api/v7/#RelatedSearches"
-          }
-        }
-      ]
-    }
-  }
-};
 
 class App extends Component {
   constructor() {
@@ -137,13 +18,13 @@ class App extends Component {
     fetch(`https://api.cognitive.microsoft.com/bing/v7.0/search?q=${this.state.querySearch}`, {
       headers: {
         "Content-Type": "application/json",
-        "Ocp-Apim-Subscription-Key": "d1164832b71449cebc47ee3fe7b565b6"
+        "Ocp-Apim-Subscription-Key": "4f21dfbfde98458c936f99435b822b35"
       },
       mode: "cors"
     }).then(function(response) {
-      return moskResult;
+      return response.json();
     }).then((results) => {
-      const resultsParsed = results.webPages.value[0].deepLinks.map((valueSearch) => {
+      const resultsParsed = results.webPages.value.map((valueSearch) => {
         return {
           title: valueSearch.name,
           description: valueSearch.snippet,
